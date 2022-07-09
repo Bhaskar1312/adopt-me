1a092dfe29ef9569c2230174ab1b0f3ab80543c1
Bhaskar.Bantupalli <bhaskarbnms0@gmail.com>
2022-07-09 23:48:07 +0530

    03-jsx

--- src/app.js
+++ src/app.js
@@ -1,22 +1,15 @@
-// /* global React ReactDOM */
-import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-  ]);
-};
+  return (
+    <div>
+      <h1>Adopt me</h1>
+      <Pet name = "Luna" animal = "dog" breed="Havanese" />
+      <Pet name = "Pepper" animal = "bird" breed="Cockateil" />
+      <Pet name = "Doink" animal = "cat" breed="Mix" />
+    </div>
+  )
+}
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
