31ce2644e86e0dc1096928caffe7a7ec71ace06f
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-11-25 22:12:43 +0530

    class components

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,66 @@
 import {useParams} from "react-router-dom";
+import {Component} from "react";
 
-const Details = () => {
-    const {id} = useParams()
-    return <h2>Hi {id}</h2>;
+// const Details = () => {
+//     const {id} = useParams()
+//     return <h2>Hi {id}</h2>;
+// }
+
+class Details extends Component {
+    // cant use Hooks, useParams in class components
+
+    constructor(props) {
+        super(props); // pass props to React
+
+        this.state = {loading: true}; // manage state in class components
+    }
+
+    async componentDidMount() { // => useEffect(()=>{}, []) in functional components
+        // runs after render
+        // can make async, need not be async
+        const res = await fetch(`http://pets-v2.dev-apis.com/pets?id=${this.props.id}`);
+        const json = await res.json();
+        // this.setState({loading: false});
+        // this.setState(json.pets[0]);
+/*
+        setTimeout(function(){
+            console.log("this", this); // this is not the class instance, but the global object
+        }, 5000);
+
+        setTimeout(() => {
+            console.log("this", this); // this is the class instance, has class context
+        })
+
+ */
+
+        // this.setState(Object.assign({loading: false}, json.pets[0])); // object spread operator
+        this.setState({loading: false, ...json.pets[0]}); // object spread operator
+    }
+
+    render() {
+        if(this.state.loading) {
+            return <h2>Loading...</h2>;
+        }
+        const {animal, breed, city, state, description, name} = this.state;
+
+        return (
+            <div className={"details"}>
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+                    <button>Adopt {name}</button>
+                    <p>{description}</p>
+                </div>
+            </div>
+        )
+    }
+
 }
 
-export default Details;
+const WrappedDetails = () => {
+    const {id} = useParams();
+    return <Details id={id} />;
+}
+
+export default WrappedDetails;
+// export default WithRouter(Details); // deprecated, higher order component
