63ea710d3f1971daadb255918908a2e7e2d2ea16
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-11-29 14:09:52 +0530

    ComponentDidUpdate

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,11 +1,12 @@
 import {Component}  from "react";
-import {Link} from "react-router-dom";
+import {Link, Navigate} from "react-router-dom";
+import {render} from "react-dom";
 
 // error boundaries are class components, cant use hooks
 // if use need error boundaries in functional components, use class components
 
 class ErrorBoundary extends Component {
-    state = {hasError: false};
+    state = {hasError: false, redirect: false};
     static getDerivedStateFromError() {
         return {hasError: true};
     }
@@ -14,8 +15,17 @@ class ErrorBoundary extends Component {
         console.error(error, errorInfo);
     }
 
+    componentDidUpdate(prevProps, prevState, snapshot) { // === useEffect with dependencies, doesn't run on first render
+        if (this.state.hasError) {
+            setTimeout(() => this.setState({redirect: true}), 5000);
+        }
+    }
+
     render() {
-        if(this.state.hasError) {
+
+        if(this.state.redirect) {
+            return <Navigate to="/"/>; // Navigate = Redirect
+        } else if(this.state.hasError) {
             return (
                 <h2>
                     This listing has an error. <Link to="/">Click here</Link> to go back to the home page.
@@ -24,6 +34,6 @@ class ErrorBoundary extends Component {
             )
         }
         return this.props.children; // just return whatever is enclosed by the error boundary
-    }
+    };
 }
 export default ErrorBoundary;
