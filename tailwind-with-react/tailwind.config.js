1ff07b0c73ef83d24d605dce5c9584cab7dbe1a6
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-12-01 11:50:33 +0530

    tailwind initial

---
+++ tailwind-with-react/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mod: 'jit', // Just-in-Time mode
+  content: ["./src/*.{html,js}"], // add the files to watch
+  theme: {
+    extend: {},
+  },
+  variant: {}, // will fill later
+  plugins: [],
+}
